6b7d5ad655380a5d30f8ab00c9af3d40bd011034
KlaidasCodes <klaidaspython@gmail.com>
2025-08-18 13:07:45 +0100

    minimum functionality is emerging

--- main.js
+++ main.js
@@ -6,8 +6,14 @@ var difficultyNow = "Easy";
 
 
 // user selects all the arithmetic signs they want used in the test
+const arrOfSigns = ["addition", "subtraction"];
+// arrOfSigns = ["addition", "subtraction", "multiplication", "division"];
 
-difficultyToLength = {
+
+
+
+
+const difficultyToLength = {
     "vEasy": 10,
     "Easy": 100,
     "Medium": 1000,
@@ -16,14 +22,52 @@ difficultyToLength = {
 
 
 
-function pickRandomNumber(difficulty_level) {
-    var multiplier = difficultyToLength[difficultyNow] 
-    var randomNumber = Math.ceil((Math.random()) * multiplier);
-    console.log(randomNumber);
+
+
+function adding(num1, num2) {
+    return num1 + num2
+}
+
+function subtracting(num1, num2) {
+    return num1 - num2
+}
+
+function multiplying(num1, num2) {
+    return num1 * num2
+}
+
+function division(num1, num2) {
+    return num1 / num2
 }
 
 
+const wordToSign = {
+    "addition": adding,
+    "subtraction": subtracting,
+    "multilpication": multiplying,
+    "division": division,
+}
+
+function pickRandomNumber(difficultyLevel) {
+    var multiplier = difficultyToLength[difficultyLevel] 
+    var randomNumber = Math.ceil((Math.random()) * multiplier);
+    console.log(randomNumber);
+    return randomNumber
+}
+
+function pickRandomSign(signArray) {
+    var randomIndex = Math.floor(Math.random() * signArray.length);
+    return signArray[randomIndex]
+}
 
 
+function creatingCalculation(difficultyLevel) {
+    var number1 = pickRandomNumber(difficultyLevel);
+    var number2 = pickRandomNumber(difficultyLevel);
+    var result = wordToSign["addition"](number1, number2);
+    console.log(result);
 
-pickRandomNumber()
+}
+
+
+creatingCalculation("Easy")
